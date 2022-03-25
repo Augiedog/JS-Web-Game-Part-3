697d2508f0241cb7c7dde95091cafbe7469c81af
Augie Bren <Augie>
2022-03-24 21:27:21 -0500

    needs more work

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
+}
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
 }
 
 newImage('assets/green-character.gif', 100, 250)
